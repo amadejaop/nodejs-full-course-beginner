f2259e5e2e28da7510158599975f133b9778d0a2
Amadeja <obsteter.amadeja@gmail.com>
2023-11-23 15:43:41 +0100

    first commit

--- 11/server.js
+++ 11/server.js
@@ -6,10 +6,17 @@ const corsOptions = require('./config/corsOptions');
 // import logger from the logEvents file
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
+
+// handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
 // cross origin resource sharing
 app.use(cors(corsOptions));
@@ -20,6 +27,9 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json
 app.use(express.json());
 
+// middleware for cookies
+app.use(cookieParser());
+
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,6 +37,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+// works like a waterfall:
+// everything after this line will use the verifyJWT middleware
+app.use(verifyJWT); 
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
